f6bf54cc246d8e13c7a66a8c47c9b24a3a9550ff
Kouhei Sutou <kou@clear-code.com>
2018-01-10 18:32:37 +0900

    Use more meaningful name

--- lib/arrow-activerecord/arrowable.rb
+++ lib/arrow-activerecord/arrowable.rb
@@ -5,6 +5,7 @@ module ArrowActiveRecord
     def to_arrow
       target_column_names = select_values
       target_column_names = column_names if select_values.empty?
+
       arrays = generate_arrow_arrays(target_column_names)
       fields = target_column_names.collect.with_index do |name, i|
         Arrow::Field.new(name, arrays[i].value_data_type)
@@ -16,26 +17,26 @@ module ArrowActiveRecord
 
     private
     def generate_arrow_arrays(target_column_names)
-      column_records = pluck(*target_column_names).transpose
+      column_values = pluck(*target_column_names).transpose
 
       target_column_names.map.with_index do |column_name, idx|
         type = columns.find { |column| column.name == column_name.to_s }.type
         case type
         when :integer
           builder = Arrow::IntArrayBuilder.new
-          builder.build(column_records[idx])
+          builder.build(column_values[idx])
         when :float
-          Arrow::FloatArray.new(column_records[idx])
+          Arrow::FloatArray.new(column_values[idx])
         when :date
-          Arrow::Date32Array.new(column_records[idx])
+          Arrow::Date32Array.new(column_values[idx])
         when :datetime
           data_type = Arrow::TimestampDataType.new(:nano)
           builder = Arrow::TimestampArrayBuilder.new(data_type)
-          builder.build(column_records[idx])
+          builder.build(column_values[idx])
         when :boolean
-          Arrow::BooleanArray.new(column_records[idx])
+          Arrow::BooleanArray.new(column_values[idx])
         else
-          Arrow::StringArray.new(column_records[idx])
+          Arrow::StringArray.new(column_values[idx])
         end
       end
     end
