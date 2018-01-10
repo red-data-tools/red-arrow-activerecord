4e0784351b82cb64b847c082617004bcd11160db
Kouhei Sutou <kou@clear-code.com>
2018-01-10 17:35:00 +0900

    Remove needless included

--- lib/arrow-activerecord/active_record_ext.rb
+++ lib/arrow-activerecord/active_record_ext.rb
@@ -2,44 +2,42 @@ require 'arrow'
 
 module ArrowActiveRecord
   module ActiveRecordExt
-    def self.included(_)
-      def to_arrow
-        @target_column_names = self.select_values.size.zero? ? self.column_names : self.select_values
-        schema = generate_schema
-        records = generate_columns
+    def to_arrow
+      @target_column_names = self.select_values.size.zero? ? self.column_names : self.select_values
+      schema = generate_schema
+      records = generate_columns
 
-        Arrow::RecordBatch.new(schema, self.size, records)
-      end
+      Arrow::RecordBatch.new(schema, self.size, records)
+    end
 
-      private
+    private
 
-      def generate_schema
-        fields = @target_column_names.map do |column_name|
-          type = self.columns.find { |e| e.name == column_name.to_s }.type
-          if type == :integer
-            Arrow::Field.new(column_name,  :int64)
-          elsif type == :float
-            Arrow::Field.new(column_name,  :float)
-          else
-            Arrow::Field.new(column_name,  :string)
-          end
+    def generate_schema
+      fields = @target_column_names.map do |column_name|
+        type = self.columns.find { |e| e.name == column_name.to_s }.type
+        if type == :integer
+          Arrow::Field.new(column_name,  :int64)
+        elsif type == :float
+          Arrow::Field.new(column_name,  :float)
+        else
+          Arrow::Field.new(column_name,  :string)
         end
-
-        Arrow::Schema.new(fields)
       end
 
-      def generate_columns
-        column_records = self.pluck(*@target_column_names).transpose
+      Arrow::Schema.new(fields)
+    end
 
-        @target_column_names.map.with_index do |column_name, idx|
-          type = self.columns.find { |e| e.name == column_name.to_s }.type
-          if type == :integer
-            Arrow::UInt64Array.new(column_records[idx])
-          elsif type == :float
-            Arrow::FloatArray.new(column_records[idx])
-          else
-            Arrow::StringArray.new(column_records[idx])
-          end
+    def generate_columns
+      column_records = self.pluck(*@target_column_names).transpose
+
+      @target_column_names.map.with_index do |column_name, idx|
+        type = self.columns.find { |e| e.name == column_name.to_s }.type
+        if type == :integer
+          Arrow::UInt64Array.new(column_records[idx])
+        elsif type == :float
+          Arrow::FloatArray.new(column_records[idx])
+        else
+          Arrow::StringArray.new(column_records[idx])
         end
       end
     end
