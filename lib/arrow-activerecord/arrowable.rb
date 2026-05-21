a3b70a838ea583d5d1da18c2363923eebb5cd799
Sutou Kouhei <kou@clear-code.com>
2026-05-22 06:37:10 +0900

    Add each_record_batch

--- lib/arrow-activerecord/arrowable.rb
+++ lib/arrow-activerecord/arrowable.rb
@@ -3,6 +3,31 @@ require 'arrow'
 module ArrowActiveRecord
   module Arrowable
     def to_arrow(batch_size: 10000)
+      record_batches = each_record_batch(batch_size:).to_a
+      Arrow::Table.new(record_batches.first.schema, record_batches)
+    end
+
+    def each_record_batch(batch_size: 10000, &block)
+      return to_enum(__method__, batch_size:) unless block_given?
+
+      schema = build_arrow_schema
+      target_column_names = schema.fields.collect(&:name)
+      record_batch_builder = Arrow::RecordBatchBuilder.new(schema)
+      in_batches(of: batch_size).each do |relation|
+        records = relation.pluck(*target_column_names)
+        if target_column_names.size == 1
+          # [1, 2, 3] ->
+          # [[1], [2], [3]]
+          record_batch_builder.append(records.collect {|value| [value]})
+        else
+          record_batch_builder.append(records)
+        end
+        yield(record_batch_builder.flush)
+      end
+    end
+
+    private
+    def build_arrow_schema
       target_column_names = select_values
       target_column_names = column_names if select_values.empty?
 
@@ -14,25 +39,9 @@ module ArrowActiveRecord
         end
         fields << {name: name, data_type: extract_arrow_data_type(target_column)}
       end
-      schema = Arrow::Schema.new(fields)
-
-      record_batches = []
-      record_batch_builder = Arrow::RecordBatchBuilder.new(schema)
-      in_batches(of: batch_size).each do |relation|
-        records = relation.pluck(*target_column_names)
-        if target_column_names.size == 1
-          # [1, 2, 3] ->
-          # [[1], [2], [3]]
-          record_batch_builder.append(records.collect {|value| [value]})
-        else
-          record_batch_builder.append(records)
-        end
-        record_batches << record_batch_builder.flush
-      end
-      Arrow::Table.new(schema, record_batches)
+      Arrow::Schema.new(fields)
     end
 
-    private
     def extract_arrow_data_type(column)
       type = nil
       if column
--- test/arrowable_test.rb
+++ test/arrowable_test.rb
@@ -11,72 +11,116 @@ class ArrowableTest < Test::Unit::TestCase
     FileUtils.rm_rf(DB_PATH.dirname)
   end
 
+  setup do
+    @date_value = Date.new(2018, 1, 10)
+    @datetime_value = Time.iso8601("2018-01-10T18:05:01.1Z")
+    @max_bigint_value = 2 ** 63 - 1
+    @min_bigint_value = -(2 ** 63)
+    ActiveRecord::Base.connection.create_table(:data) do |table|
+      table.string :string_column
+      table.date :date_column
+      table.datetime :datetime_column
+      table.boolean :boolean_column
+      table.bigint :bigint_column
+    end
+    Data.create(string_column: "Hello",
+                date_column: @date_value,
+                datetime_column: @datetime_value,
+                boolean_column: false,
+                bigint_column: @max_bigint_value)
+    Data.create(string_column: "Hello2",
+                date_column: @date_value + 1,
+                datetime_column: @datetime_value + 1,
+                boolean_column: true,
+                bigint_column: @min_bigint_value)
+  end
+
+  teardown do
+    ActiveRecord::Base.connection.drop_table(:data)
+  end
+
   sub_test_case("#to_arrow") do
-    setup do
-      @date_value = Date.new(2018, 1, 10)
-      @datetime_value = Time.iso8601("2018-01-10T18:05:01.1Z")
-      @bigint_value = 2 ** 63 - 1
-      ActiveRecord::Base.connection.create_table(:data) do |table|
-        table.string :string_column
-        table.date :date_column
-        table.datetime :datetime_column
-        table.boolean :boolean_column
-        table.bigint :bigint_column
-      end
-      Data.create(string_column: "Hello",
-                  date_column: @date_value,
-                  datetime_column: @datetime_value,
-                  boolean_column: false,
-                  bigint_column: @bigint_value)
-      Data.create(string_column: "Hello2",
-                  date_column: @date_value + 1,
-                  datetime_column: @datetime_value + 1,
-                  boolean_column: true,
-                  bigint_column: -@bigint_value - 1)
+    test "one column" do
+      table = Data.all.select(:id).to_arrow(batch_size: 1)
+      assert_equal([
+                     Arrow::RecordBatch.new(id: Arrow::Int32Array.new([1])),
+                     Arrow::RecordBatch.new(id: Arrow::Int32Array.new([2])),
+                   ],
+                   table.each_record_batch.to_a)
     end
 
-    teardown do
-      ActiveRecord::Base.connection.drop_table(:data)
+    test "all columns" do
+      table = Data.all.to_arrow(batch_size: 1)
+      assert_equal([
+                     Arrow::RecordBatch.new(
+                       id: Arrow::Int32Array.new([1]),
+                       string_column: Arrow::StringArray.new(["Hello"]),
+                       date_column: Arrow::Date32Array.new([@date_value]),
+                       datetime_column: Arrow::TimestampArray.new(
+                         :nano,
+                         [@datetime_value]
+                       ),
+                       boolean_column: Arrow::BooleanArray.new([false]),
+                       bigint_column:
+                         Arrow::Int64Array.new([@max_bigint_value]),
+                     ),
+                     Arrow::RecordBatch.new(
+                       id: Arrow::Int32Array.new([2]),
+                       string_column: Arrow::StringArray.new(["Hello2"]),
+                       date_column: Arrow::Date32Array.new([@date_value + 1]),
+                       datetime_column: Arrow::TimestampArray.new(
+                         :nano,
+                         [@datetime_value + 1],
+                       ),
+                       boolean_column: Arrow::BooleanArray.new([true]),
+                       bigint_column:
+                         Arrow::Int64Array.new([@min_bigint_value]),
+                     ),
+                   ],
+                   table.each_record_batch.to_a)
+    end
+  end
+
+  sub_test_case("#each_record_batch") do
+    test "one column" do
+      record_batches =
+        Data.all.select(:id).each_record_batch(batch_size: 1).to_a
+      assert_equal([
+                     Arrow::RecordBatch.new(id: Arrow::Int32Array.new([1])),
+                     Arrow::RecordBatch.new(id: Arrow::Int32Array.new([2])),
+                   ],
+                   record_batches)
     end
 
-    test "one" do
-      arrow = Data.all.select(:id).to_arrow
-      assert_equal(<<-RECORD_BATCH, arrow.each_record_batch.first.to_s)
-id:   [
-    1,
-    2
-  ]
-      RECORD_BATCH
-    end
-
-    test "all" do
-      arrow = Data.all.to_arrow
-      assert_equal(<<-RECORD_BATCH, arrow.each_record_batch.first.to_s)
-id:   [
-    1,
-    2
-  ]
-string_column:   [
-    "Hello",
-    "Hello2"
-  ]
-date_column:   [
-    #{@date_value},
-    #{@date_value + 1}
-  ]
-datetime_column:   [
-    #{@datetime_value.strftime("%Y-%m-%d %H:%M:%S.%9N")},
-    #{(@datetime_value + 1).strftime("%Y-%m-%d %H:%M:%S.%9N")}
-  ]
-boolean_column:   [
-    false,
-    true
-  ]
-bigint_column:   [
-    #{@bigint_value},
-    #{-@bigint_value - 1}
-  ]
-      RECORD_BATCH
+    test "all columns" do
+      record_batches = Data.all.each_record_batch(batch_size: 1).to_a
+      assert_equal([
+                     Arrow::RecordBatch.new(
+                       id: Arrow::Int32Array.new([1]),
+                       string_column: Arrow::StringArray.new(["Hello"]),
+                       date_column: Arrow::Date32Array.new([@date_value]),
+                       datetime_column: Arrow::TimestampArray.new(
+                         :nano,
+                         [@datetime_value]
+                       ),
+                       boolean_column: Arrow::BooleanArray.new([false]),
+                       bigint_column:
+                         Arrow::Int64Array.new([@max_bigint_value]),
+                     ),
+                     Arrow::RecordBatch.new(
+                       id: Arrow::Int32Array.new([2]),
+                       string_column: Arrow::StringArray.new(["Hello2"]),
+                       date_column: Arrow::Date32Array.new([@date_value + 1]),
+                       datetime_column: Arrow::TimestampArray.new(
+                         :nano,
+                         [@datetime_value + 1],
+                       ),
+                       boolean_column: Arrow::BooleanArray.new([true]),
+                       bigint_column:
+                         Arrow::Int64Array.new([@min_bigint_value]),
+                     ),
+                   ],
+                   record_batches)
     end
   end
 end
