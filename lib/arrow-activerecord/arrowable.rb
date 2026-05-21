0d4891aeee621cf8a480b3237cca6dfb75bcfbe5
Sutou Kouhei <kou@clear-code.com>
2026-05-22 05:48:51 +0900

    Add an example comment

--- lib/arrow-activerecord/arrowable.rb
+++ lib/arrow-activerecord/arrowable.rb
@@ -21,6 +21,8 @@ module ArrowActiveRecord
       in_batches(of: batch_size).each do |relation|
         records = relation.pluck(*target_column_names)
         if target_column_names.size == 1
+          # [1, 2, 3] ->
+          # [[1], [2], [3]]
           record_batch_builder.append(records.collect {|value| [value]})
         else
           record_batch_builder.append(records)
